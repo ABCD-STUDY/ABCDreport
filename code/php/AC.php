a2a147a4abf4d96d17fc82f2fba1079992a6bf6a
Alex DeCastro <alexdecastro2@gmail.com>
2016-01-23 23:43:59 -0800

    Change absolute path to relative

--- code/php/AC.php
+++ code/php/AC.php
@@ -485,9 +485,9 @@ syslog(LOG_EMERG, 'try to remove permission '.$name.' ('.$id.') from '.$role['na
       	$qs = $_SERVER['QUERY_STRING'];
         audit( "check_logged failed", "" );
         if ($qs != "")
-           header("Location: /ABCDreport/applications/User/login.php".$_SERVER['QUERY_STRING']."&url=".$_SERVER['PHP_SELF']);
+           header("Location: ../../applications/User/login.php".$_SERVER['QUERY_STRING']."&url=".$_SERVER['PHP_SELF']);
         else
-           header("Location: /ABCDreport/applications/User/login.php"."?url=".$_SERVER['PHP_SELF']);
+           header("Location: ../../applications/User/login.php"."?url=".$_SERVER['PHP_SELF']);
      };
      // store that this user has logged in now
      setUserVariable( $_SESSION["logged"], "lastTimeLoggedIn", date(DATE_RFC2822) );
