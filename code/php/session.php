dcbc9363137ae4e4d34644a3f7c0094c64f2135a
Alex DeCastro <alexdecastro2@gmail.com>
2016-01-28 19:38:33 -0800

    update with storage in session variable

---
+++ code/php/session.php
@@ -0,0 +1,15 @@
+<?php
+
+  session_start();
+
+  include($_SERVER["DOCUMENT_ROOT"]."/code/php/AC.php");
+  $user_name = check_logged(); /// function checks if visitor is logged.
+
+  if (isset($_GET['subjid'])) {
+    $_SESSION['subjid'] = $_GET['subjid'];
+  }
+  if (isset($_GET['session'])) {
+    $_SESSION['session'] = $_GET['session'];
+  }
+  
+?>
