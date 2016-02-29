0fcd5eb78cf836451e80915d0f4d0dc62fd5ac19
Alex DeCastro <alexdecastro2@gmail.com>
2016-02-29 12:49:22 -0800

    store any number of keys for a task specific session

--- code/php/session.php
+++ code/php/session.php
@@ -1,18 +1,39 @@
 <?php
 
+  // store session variables as $_SESSION['ABCD'] = [ "TaskName" => [ "key" => "value", ] ]
+  // expected argument is $_GET['task'] the name of the task
+
   session_start();
 
   include($_SERVER["DOCUMENT_ROOT"]."/code/php/AC.php");
   $user_name = check_logged(); /// function checks if visitor is logged.
 
-  if (isset($_GET['subjid'])) {
-    $_SESSION['subjid'] = $_GET['subjid'];
+  // assume that we need subjectid, sessionid, and task
+  $ar = array();
+  if (isset($_SESSION['ABCD'])) {
+     $ar = $_SESSION['ABCD'];
   }
-  if (isset($_GET['session'])) {
-    $_SESSION['sessionid'] = $_GET['session'];
+
+  $task = "";
+  if (isset($_GET['task'])) {
+     $task = $_GET['task'];
+  } else {
+     echo ("ERROR: no task specified, cannot do anything");
+     return;
   }
-  if (isset($_GET['act_subst'])) {
-    $_SESSION['act_subst'] = $_GET['act_subst'];
+
+  // all values we have to store (could be subjid, sessionid, etc.)
+  $vals = array();
+  foreach($_GET as $key => $value) {
+     if ($key == "task") {
+       continue;
+     }
+     $vals[$key] = $value;
   }
-  
+  if (!array_key_exists($task, $ar)) {
+     $ar[$task] = array();
+  }
+  $ar[$task] = array_merge($ar[$task], $vals);
+
+  $_SESSION['ABCD'] = $ar;
 ?>
